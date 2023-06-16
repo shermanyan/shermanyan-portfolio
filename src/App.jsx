a07728f8772bc3fe38ef2845fd817dc1ed2ef382
shermanyan <syan1689@gmail.com>
2023-06-15 18:38:40 -0700

    add logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import linkedInLogo from './assets/linkedin.svg'
+import gmailLogo from './assets/gmail.svg'
+
 import './App.css'
 
 function App() {
@@ -9,7 +12,7 @@ function App() {
   const text = "null"
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -34,9 +37,30 @@ function App() {
                         LinkedIn
           </a>
       </div>
+
+      <iframe className='figma' width = {1000} height = {1000} allowfullscreen src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FmZNYAybmni55Zf7WwU0KTX%2FDJ-App-Prototype%3Ftype%3Ddesign%26node-id%3D0%253A1%26t%3DwPjCMujzHFEkLLf0-1"></iframe> */}
+
+      <div>
+        <h1 className='title'>
+          Sherman Yan 
+        </h1>
+      </div>
+        <a className='icons'>
+        <a className = 'linkedin' onClick= {() => open("https://www.linkedin.com/in/sherman-yan-85847a184/")}>
+          <img src = {linkedInLogo} width={100}/>
+        </a>
+        <a className = 'gmail' onClick= {() => open("mailto:shermanyanofficial@gmail.com")}>
+          <img src = {gmailLogo}width={100}/>
+        </a>
+        </a>
+    <div>
+      </div>
     </div>
-  )
-}
+    )
+    }
+  
+
+
 
 
 export default App
